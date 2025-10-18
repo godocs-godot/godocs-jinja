f3a471fbb2907346a2b0cbc589b9a8d82214d8b2
Daniel O. Sousa <97067805+nadjiel@users.noreply.github.com>
2025-10-17 23:21:26 -0300

    :sparkles: feat: moved contructor code to its own command

--- src/godocs_jinja/cli/command.py
+++ src/godocs_jinja/cli/command.py
@@ -1,15 +1,12 @@
 from argparse import ArgumentParser, Namespace
 from typing import cast, TYPE_CHECKING, Optional
 from godocs.cli.command import CLICommand
-from godocs.parser import xml_parser, context_creator
-from godocs.parser.context_creator import DocContext
-from godocs.translation.interpreter import Interpreter, BBCodeInterpreter
-from godocs.translation.translator import get_translator, SyntaxTranslator
 from godocs_jinja.constructor import JinjaConstructor
 from godocs.constructor.constructor import ConstructorContext
 
 if TYPE_CHECKING:
     from argparse import _SubParsersAction  # type: ignore
+    from godocs.cli.command.cli_command import Processor
 
 
 class JinjaCommand(CLICommand):
@@ -33,60 +30,11 @@ class JinjaCommand(CLICommand):
     The `subparsers` of the `parser` of this `AppCommand`.
     """
 
-    # TODO: move to dedicated place, and organize better
-    def translate_ctx(self, ctx: DocContext, interpreter: Interpreter, translator: SyntaxTranslator):
-        classes = ctx["classes"]
-        for class_doc in classes:
-            for member in class_doc:
-                match(member):
-                    case "brief_description":
-                        class_doc[member] = interpreter.interpret(
-                            class_doc[member]).translate(translator)
-                    case "description":
-                        class_doc[member] = interpreter.interpret(
-                            class_doc[member]).translate(translator)
-                    case "constants":
-                        constants = class_doc[member]
-                        for constant in constants:
-                            constant["description"] = interpreter.interpret(
-                                constant["description"]).translate(translator)
-                    case "enums":
-                        enums = class_doc[member]
-                        for enum in enums:
-                            enum["description"] = interpreter.interpret(
-                                enum["description"]).translate(translator)
-                            constants = enum["values"]
-                            for constant in constants:
-                                constant["description"] = interpreter.interpret(
-                                    constant["description"]).translate(translator)
-                    case "methods":
-                        methods = class_doc[member]
-                        for method in methods:
-                            method["description"] = interpreter.interpret(
-                                method["description"]).translate(translator)
-                    case "properties":
-                        properties = class_doc[member]
-                        for property in properties:
-                            property["description"] = interpreter.interpret(
-                                property["description"]).translate(translator)
-                    case "signals":
-                        signals = class_doc[member]
-                        for signal in signals:
-                            signal["description"] = interpreter.interpret(
-                                signal["description"]).translate(translator)
-                    case "theme_items":
-                        theme_items = class_doc[member]
-                        for theme_item in theme_items:
-                            theme_item["description"] = interpreter.interpret(
-                                theme_item["description"]).translate(translator)
-                    case _: pass
-
-        return ctx
-
     def register(
         self,
         superparsers: "Optional[_SubParsersAction[ArgumentParser]]" = None,
         parent_parser: Optional[ArgumentParser] = None,
+        processors: "Optional[list[Processor]]" = None
     ):
         """
         Registers this `JinjaCommand` as a subparser for the
@@ -126,16 +74,6 @@ class JinjaCommand(CLICommand):
         Executes the main logic of this command with the parsed `args`.
         """
 
-        docs = xml_parser.parse(args.input_dir)
-
-        ctx = context_creator.create(docs)
-
-        interpreter = BBCodeInterpreter()
-
-        translator = get_translator(args.translator)
-
-        ctx = self.translate_ctx(ctx, interpreter, translator)
-
         constructor = JinjaConstructor(
             model=args.model,
             templates_path=args.templates,
@@ -144,4 +82,5 @@ class JinjaCommand(CLICommand):
             output_format="rst",  # TODO: make format customizable
         )
 
-        constructor.construct(cast(ConstructorContext, ctx), args.output_dir)
+        constructor.construct(
+            cast(ConstructorContext, args.ctx), args.output_dir)
