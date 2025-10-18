02ba1eb2eac317f3e42cdd40db73ec2b0be1fdfe
Daniel O. Sousa <97067805+nadjiel@users.noreply.github.com>
2025-10-17 21:58:09 -0300

    :sparkles: feat: added plugin registration

--- src/godocs_jinja/main.py
+++ src/godocs_jinja/main.py
@@ -1,9 +1,16 @@
-from godocs.plugin import Plugin
-from godocs.cli import CLICommand
+from godocs.plugin import Plugin as BasePlugin
+from godocs.cli import AppCommand
+from godocs_jinja.cli.command import JinjaCommand
 
 
-class JinjaPlugin(Plugin):
+class JinjaPlugin(BasePlugin):
 
-    def register(self, app: CLICommand):
-        print("Plugin registered: godocs-jinja")
-        pass
+    def register(self, app: AppCommand):
+        construct = app.subcommands['construct']
+
+        construct.subcommands["jinja"] = JinjaCommand()
+
+        construct.subcommands["jinja"].register(
+            construct.subparsers,
+            construct.parent_parser
+        )
